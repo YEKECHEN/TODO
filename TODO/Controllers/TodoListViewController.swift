e441cbb567e638e9e5aef6758397bdd00ba16688
yekechen <452964644@qq.com>
2019-04-30 14:45:32 +0800

    111

--- TODO/Controllers/TodoListViewController.swift
+++ TODO/Controllers/TodoListViewController.swift
@@ -9,23 +9,55 @@
 import UIKit
 
 class TodoListViewController: UITableViewController{
+    
+    var itemArray = [Item]()
+    
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
+    func loadItems(){
+        if let data = try? Data(contentsOf: dataFilePath!){
+            let decoder = PropertyListDecoder()
+            do{
+                itemArray = try decoder.decode([Item].self, from: data)
+            }catch {
+                print("解码item错误！")
+            }
+        }
+    }
+    
+    func saveItem(){
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        }catch{
+            print("编译错误\(error)")
+        }
+    }
 
-    let defaults = UserDefaults.standard
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print(dataFilePath!)
         
-        if let items = defaults.array(forKey: "ToDoListArray") as? [String]{
-            itemArray = items
-        }
+        loadItems()
     }
-var itemArray = ["购买水杯","吃药","修改密码"]
     
     //MARK: - Table View DataSource methods
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        cell.textLabel?.text = itemArray[indexPath.row].title
         
+        var item = itemArray[indexPath.row]
+        
+        cell.accessoryType = item.done == true ? .checkmark : .none
+//        if itemArray[indexPath.row].done == false{
+//            cell.accessoryType = .none
+//        }else{
+//            cell.accessoryType = .checkmark
+//        }
+//
         return cell
     }
     
@@ -36,11 +68,22 @@ var itemArray = ["购买水杯","吃药","修改密码"]
     //MARK:  - Table View Delegate methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }else{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if itemArray[indexPath.row].done == false {
+            itemArray[indexPath.row].done = true
+        }else {
+            itemArray[indexPath.row].done = false
         }
+        
+        tableView.beginUpdates()
+        tableView.reloadRows(at: [indexPath], with: UITableView.RowAnimation.automatic)
+        tableView.endUpdates()
+        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        saveItem()
+        
         tableView.deselectRow(at: indexPath, animated: true)
     }
     //MARK: - Add New Items
@@ -52,10 +95,17 @@ var itemArray = ["购买水杯","吃药","修改密码"]
     let alert = UIAlertController(title: "添加一个新的todo项目", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "添加新项目", style: .default){(action) in
             //用户单击j添加项目按钮以后要执行的代码
-            self.itemArray.append(textField.text!)
-            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
+            
+            let newItem = Item()
+            newItem.title = textField.text!
+            
+            self.itemArray.append(newItem)
+            
+            self.saveItem()
+            
             self.tableView.reloadData()
         }
+        
         alert.addTextField{(alertTextField) in
             alertTextField.placeholder = "创建一个项目..."
             //让textField指向alertTextField，因为出了闭包，alerttextfield不存在
---
+++ TODO/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  TODO
+//
+//  Created by 叶柯辰 on 2019/4/17.
+//  Copyright © 2019 yekechen. All rights reserved.
+//
+
+import Foundation
+
+class Item : Encodable,Decodable {
+    var title = ""
+    var done = false
+}
